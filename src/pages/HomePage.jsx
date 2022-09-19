ba903349d8f53b65953889d320276ef3a7814ed4
Luis Lechuga <luislechu55@gmail.com>
2022-09-19 14:55:54 -0500

    feat: changes

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -8,7 +8,7 @@ export const HomePage = ({ image, name, rating, id, status }) => {
         <div className='card-body'>
           <h5 className='card-title'>{name}</h5>
           <p className='card-text'><i className='fas fa-star' />{rating}</p>
-          <p>- {status}</p>
+          <p>{status}</p>
           <Link to={`singlepage/${id}`} className='btn btn-primary'>Information</Link>
         </div>
       </div>
